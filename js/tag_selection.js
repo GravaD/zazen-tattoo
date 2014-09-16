e89b9f8f3433c2d7db01bf4ec6ce80ad961cb189
Alexandre Faucher <allemagne7@hotmail.fr>
2014-09-16 11:59:55 +0200

    [Update] JQuery tag selection.

--- js/tag_selection.js
+++ js/tag_selection.js
@@ -7,7 +7,20 @@ function select_img(tag){
 	}
 }
 
-//ajouter la mise en évidence du tag sélectionné
+/*function select_img(){
+	$('li>a').click(function(){
+		var tag = $(this).html(); //mettre en minuscule
+		//if($('section.photo').class().contains(tag)){
+	});
+	if(tag=='tous'){
+		$('section.photos').css({'display':'block'});
+	}else{
+		$('section.photos').css({'display':'none'});
+		$('section.'+tag).css({'display':'block'});
+	}
+}*/
+
+//ajouter la mise en évidence du tag sélectionné?
 
 		//exemple:
 		//$('#trigger-gothique').click(function(){
